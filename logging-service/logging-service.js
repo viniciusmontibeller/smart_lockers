1b12a1a4f4d4afcab71cf847062957580645ccd0
Vinicius da Silva Montibeller <vinism96@hotmail.com>
2026-06-01 15:47:38 -0300

    chore: commit inicial com a estrutura de pastas dos servicos e o esqueleto do projeto

---
+++ logging-service/logging-service.js
@@ -0,0 +1,35 @@
+// Inicia o Express.js
+const express = require('express');
+const app = express();
+
+// Body Parser - usado para processar dados da requisição HTTP
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Inicia o Servidor HTTP na porta 8060? confirmar portas
+let porta = 8060;
+app.listen(porta, () => {
+ console.log('Servidor em execução na porta: ' + porta);
+});
+
+// Importa o package do SQLite
+const sqlite3 = require('sqlite3');
+
+// Acessa o arquivo com o banco de dados
+var db = new sqlite3.Database('./dados.db', (err) => {
+        if (err) {
+            console.log('ERRO: não foi possível conectar ao SQLite.');
+            throw err;
+        }
+        console.log('Conectado ao SQLite!');
+});
+
+// Estabelecer criacao de tabela. Cria a tabela cadastro, caso ela não exista
+db.run(``, 
+        [], (err) => {
+           if (err) {
+              console.log('ERRO: não foi possível criar tabela.');
+              throw err;
+           }
+      }); 
