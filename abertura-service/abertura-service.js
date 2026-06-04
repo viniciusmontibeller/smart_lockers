d1e8d33ff6a57675fde03831a491b0c1964929bd
Vinicius da Silva Montibeller <vinism96@hotmail.com>
2026-06-03 23:20:24 -0300

    feat: criado servico simples de abertura de gaveta

---
+++ abertura-service/abertura-service.js
@@ -0,0 +1,22 @@
+// Inicia o Express.js
+const express = require('express');
+const app = express();
+
+// Body Parser - usado para processar dados da requisição HTTP
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Inicia o Servidor HTTP na porta 8060? confirmar portas
+let porta = 8050;
+app.listen(porta, () => {
+ console.log('Servidor em execução na porta: ' + porta);
+});
+
+app.post('/abrir', (req, res) => {
+    const { armario_id, numero_gaveta } = req.body;
+
+    console.log(`Abrindo gaveta ${numero_gaveta} do armário ${armario_id}`);
+
+    res.status(200).send('Gaveta aberta com sucesso.');
+});
