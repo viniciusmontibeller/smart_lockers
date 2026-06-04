8fad00310f3e6c8abfd3f56462b58656ecba4fc6
Vinicius da Silva Montibeller <vinism96@hotmail.com>
2026-06-03 23:19:37 -0300

    feat: adicionado o banco e servico de logging

--- logging-service/logging-service.js
+++ logging-service/logging-service.js
@@ -17,7 +17,7 @@ app.listen(porta, () => {
 const sqlite3 = require('sqlite3');
 
 // Acessa o arquivo com o banco de dados
-var db = new sqlite3.Database('./dados.db', (err) => {
+var db = new sqlite3.Database('./logs.db', (err) => {
         if (err) {
             console.log('ERRO: não foi possível conectar ao SQLite.');
             throw err;
@@ -26,10 +26,45 @@ var db = new sqlite3.Database('./dados.db', (err) => {
 });
 
 // Estabelecer criacao de tabela. Cria a tabela cadastro, caso ela não exista
-db.run(``, 
+db.run(`CREATE TABLE IF NOT EXISTS logs (
+                entrega_id INTEGER,
+                retirado INTEGER NOT NULL CHECK (retirado IN (0, 1)),
+                cpf INTEGER,
+                armario_id INTEGER,
+                numero_gaveta INTEGER,
+                data DATE,
+                hora TIME,
+                PRIMARY KEY (entrega_id, retirado)
+        )`, 
         [], (err) => {
-           if (err) {
-              console.log('ERRO: não foi possível criar tabela.');
-              throw err;
-           }
-      }); 
+                if (err) {
+                console.log('ERRO: não foi possível criar tabela logging.');
+                throw err;
+                }
+});
+
+      
+app.post('/log', (req, res, next) => {
+        db.run(`INSERT INTO logs (entrega_id, retirado, cpf, armario_id, numero_gaveta, data, hora) VALUES(?,?,?,?,?,?,?)`,
+                [req.body.entrega, req.body.retirado, req.body.cpf, req.body.armario, req.body.numero_gaveta, req.body.data, req.body.hora], (err) => {
+                if (err) {
+                        console.log("Error: " + err);
+                        res.status(500).send('Erro ao criar entrada de log.');
+                } else {
+                        console.log('Log criado com sucesso!');
+                        res.status(200).send('Log criado com sucesso!');
+                }
+    });
+});
+
+// busca todos os log
+app.get('/log', (req, res, next) => {
+    db.all(`SELECT * FROM logs`, [], (err, result) => {
+        if (err) {
+             console.log("Erro: " + err);
+             res.status(500).send('Erro ao obter dados.');
+        } else {
+            res.status(200).json(result);
+        }
+    });
+});
